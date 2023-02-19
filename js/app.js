7eb09f4a777d015342e2559f96dac604d8355d2c
Brandon Ruiz <Sober4202020@outlook.com>
2023-02-19 06:05:54 +0000

    hard as hell

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ and the second element is a concatenated string that EXACTLY follows this exampl
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function testSum(a, b) {
+function sum(a, b) {
   let x = a + b;
   let football = `The sum of ${a} and ${b} is ${x}`;
-  const list = [x, football];
-  alert(list);
+  return [x, football];
+  
 
 }
 
@@ -24,18 +24,19 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as 
+arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function testMultiply(a, b) {
+function Multiply(a, b) {
   let x = a * b;
-  let moose = `The product of ${a} and ${9} is ${x}`;
-  const bike = [x, moose];
-  alert(bike);
+  let r = `The product of ${a} and ${b} is${x}`;
+  return [x,r ];
+  
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -45,7 +46,11 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments 
+and returns an array where the first element is the sum of those three numbers, 
+the second element is the product of those three numbers,  and the third and fourth 
+elements are strings that EXACTLY follow this example and use the values that were input 
+into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
@@ -55,12 +60,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function SumAndMultiply(a, b, c) {
+  let x = sum(a,b);
+  let xy = sum(x[0],c);
+  let h = Multiply(a, b);
+  let hr = Multiply(h[0],c);
+  let cow = `${a} and ${b} and ${c} sum to ${xy[0]}`;
+  let horse = `The product of ${a} and ${b} and ${c} is ${hr[0]}`;
+
+
+  return [xy[0], hr[0],cow, horse ];
 
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+
+// // Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
